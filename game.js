dc1c4d1d2399197b0af228bd5a00c58eec080753
genizy <185154684+genizy@users.noreply.github.com>
2025-03-01 22:26:06 -0800

    gjklghlj

--- game.js
+++ game.js
@@ -15713,7 +15713,7 @@ ig.module("plugins.gamedist").defines(function() {
     window.GD_OPTIONS = l2V;
     b = "color: blue; font-weight: bold";
     d = document.getElementsByTagName("script")[0];
-    document.getElementById(P7Q) || (c = document.createElement("script"), c.id = P7Q, c.src = "https://cdn.jsdelivr.net/gh/genizy/dwtd@983622c4b41f0c2e716edb25474c8df8ad88be70/main.min.js", d.parentNode.insertBefore(c, d));
+    document.getElementById(P7Q) || (c = document.createElement("script"), c.id = P7Q, c.src = "https://cdn.jsdelivr.net/gh/genizy/dwtd@a33cb2f220a312b0de2f06561950593bb3ed8b1e/main.min.js", d.parentNode.insertBefore(c, d));
     ig.Gd = ig.Class.extend(function() {
         var r2V;
         r2V = {};
